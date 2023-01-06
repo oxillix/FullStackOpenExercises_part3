60f232123f9ee6a78d0fa3abe3de559a772f3525
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-01-07 00:40:03 +0100

    3.12: Command-line database

---
+++ phonebook_backend/mongo.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+//getting rid of DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7
+mongoose.set("strictQuery", false);
+
+const savePersonToDB = (name, number) => {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      const person = new Person({
+        name: name,
+        number: number,
+      });
+
+      return person.save();
+    })
+    .then(() => {
+      console.log(`added ${name} number ${number} to phonebook`);
+      return mongoose.connection.close();
+    })
+    .catch((err) => console.log(err));
+};
+
+const getAllPeopleFromDB = () => {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log("phonebook:");
+
+      Person.find().then((result) => {
+        result.forEach((person) => {
+          console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+if (!process.argv[2]) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password> <name> <number>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0.lemk6ue.mongodb.net/phonebook_backend?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv[2] && !process.argv[3] && !process.argv[4]) {
+  getAllPeopleFromDB();
+} else if (process.argv[2] && process.argv[3] && process.argv[4]) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  savePersonToDB(name, number);
+}
