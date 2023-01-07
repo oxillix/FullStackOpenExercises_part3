ad844b3a11c5331ba21bf42c6b3e67d96c2ee1e8
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-01-07 20:39:17 +0100

    3.21 Deploying the database backend to production

--- phonebook_backend/models/person.js
+++ phonebook_backend/models/person.js
@@ -22,6 +22,7 @@ const personSchema = new mongoose.Schema({
     validate: {
       validator: function (v) {
         return /^\d{2,3}-\d+$/.test(v);
+        
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
